6589d5229f85b563c64cf7213f2314e4cf6a50b6
sousamacedo18 <sousamacedo18@gmail.com>
2022-08-18 20:24:51 -0300

    primeiro commit

---
+++ src/pages/cadastroUsuario/index.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function Cadastrousuario(){
+ return(
+<div>
+    <h1>estou no cadastro de usuários</h1>
+</div>
+
+ )   
+}
---
+++ src/routes.js
@@ -0,0 +1,13 @@
+import React from "react";
+import {BrowserRouter,Route,Switch} from "react-router-dom";
+import Cadastrousuario from "./pages/cadastroUsuario";
+
+export default function Routes(){
+    return(
+        <BrowserRouter>
+                <Switch>
+                    <Route path={"/cadastrousuario"} component={Cadastrousuario} />
+                </Switch>
+        </BrowserRouter>
+    )
+}
