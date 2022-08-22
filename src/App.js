ef9909a29e95ba3b0ff1edbb5859e0205c3e0e93
Informatica para Internet <infornet@sistemafieto.rede>
2022-08-22 20:48:40 -0300

    segunda alteraçao

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
+
 import './App.css';
 import Routes from './routes';
 
 function App() {
   return (
     <div className="App">
-      
       <Routes />
     </div>
   );
